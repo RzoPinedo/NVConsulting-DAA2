e47e3b79725745875670e70126a8886d99a9ccb1
RzoPinedo <rzopinedo@gmail.com>
2021-11-07 21:54:03 -0500

    actualizacion de la aplicacion en java con el modulo cursos

---
+++ src/main/java/idat/edu/pe/NvConsultingApplication.java
@@ -0,0 +1,13 @@
+package idat.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvConsultingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvConsultingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/idat/edu/pe/jpa/repositorios/EmpleadosRepositorio.java
@@ -0,0 +1,9 @@
+package idat.edu.pe.jpa.repositorios;
+
+import org.springframework.data.repository.CrudRepository;
+
+import idat.edu.pe.jpa.modelo.Empleados;
+
+public interface EmpleadosRepositorio extends CrudRepository<Empleados, Integer>{
+
+}
