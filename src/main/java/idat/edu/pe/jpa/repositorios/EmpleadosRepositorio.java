d5380e63392d9d7e189b207597e1a426e9bf3d73
RzoPinedo <rzopinedo@gmail.com>
2021-11-18 23:03:36 -0500

    correccion de archivo

--- src/main/java/idat/edu/pe/jpa/repositorios/EmpleadosRepositorio.java
+++
@@ -1,9 +0,0 @@
-package idat.edu.pe.jpa.repositorios;
-
-import org.springframework.data.repository.CrudRepository;
-
-import idat.edu.pe.jpa.modelo.Empleados;
-
-public interface EmpleadosRepositorio extends CrudRepository<Empleados, Integer>{
-
-}
